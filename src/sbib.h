4eefb9d589d98f72ec745b9c6973385c1e071e0f
RF-TQ <rafael.queypo@gmail.com>
2022-01-22 01:18:43 -0800

    commas

--- src/sbib.h
+++ src/sbib.h
@@ -13,13 +13,13 @@ static const std::string responses[SIZE] =
         "<:sbibo:864916773186175036>", 
         "<:sbiboleft:864916822679486484>",
         "A shizuku may appear if you do",
-        "Yes, but you could sbib instead"
+        "Yes, but you could sbib instead",
         "It would be better to sbib",
         "Outlook sbib",
-        "<:sbibo:864916773186175036> 👍 !!!"
-        "Only if you roll a sbib"
-        "No, but have a sbib in these trying times <:sbibo:864916773186175036>"
-        "If you sbib, it will happen"
+        "<:sbibo:864916773186175036> 👍 !!!",
+        "Only if you roll a sbib",
+        "No, but have a sbib in these trying times <:sbibo:864916773186175036>",
+        "If you sbib, it will happen",
         "<:sbiboangery:864916789258747946>"
     };
 
